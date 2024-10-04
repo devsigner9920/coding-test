a3215124faccb4291eefb0b0b73f30955b2f36e5
강원준 <38320469+devsigner9920@users.noreply.github.com>
2024-10-04 21:48:56 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static long[][] D = new long[91][2];
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var N = Integer.parseInt(br.readLine());
+
+        D[1][1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            D[i][0] = D[i - 1][0] + D[i - 1][1];
+            D[i][1] = D[i - 1][0];
+        }
+
+        System.out.println(D[N][0] + D[N][1]);
+    }
+}
