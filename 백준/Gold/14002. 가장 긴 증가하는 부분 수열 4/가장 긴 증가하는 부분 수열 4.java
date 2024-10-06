649e58516b56b0858d44f12107b1d30e43c25e8b
강원준 <38320469+devsigner9920@users.noreply.github.com>
2024-10-06 17:08:08 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 148 ms, Memory: 16472 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 입력된 수열 저장
+    private static int[] A = new int[10_000];
+    private static int[] D = new int[10_000];
+    private static int[] V = new int[10_000];
+
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var N = Integer.parseInt(br.readLine());
+        var st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        V[0] = -1;
+
+        for (int i = 0; i < N; i++) {
+            D[i] = 1;
+            V[i] = -1;
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j] && D[i] < D[j] + 1) {
+                    D[i] = D[j] + 1;
+                    V[i] = j;
+                }
+            }
+        }
+
+        int temp = 0;
+        int tempIdx = 0;
+        for (int i = 0; i < N; i++) {
+            if (temp < D[i]) {
+                temp = D[i];
+                tempIdx = i;
+            }
+        }
+
+        var a= go(tempIdx).trim();
+        System.out.println(temp);
+        System.out.println(a);
+    }
+
+    private static String go(int n) {
+        if (n == -1) {
+            return "";
+        }
+        return go(V[n]) + " " + A[n];
+    }
+}
