250a653591b3e14fd9744b0b06cbb9f110d00345
강원준 <devsigner9920@icloud.com>
2024-11-02 10:17:24 +0900

    [Silver II] Title: 부분수열의 합, Time: 116 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,56 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int N;
-    private static int M;
-    private static int S;
-    private static int[] arr;
+    private static int[] a;
+    private static int s;
     private static int count;
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        var st2 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
-        }
-        for (int i = 1; i <= N; i++) {
-            M = i;
-            var result = new int[M];
-            go(0, 0, result);
+        var n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        bf(0, 0);
+        if (s == 0) {
+            count--;
         }
         System.out.println(count);
     }
 
-    private static void go(int start, int depth, int[] result) {
-        if (depth == M) {
-            if (S == Arrays.stream(result).sum()) {
+    private static void bf(int index, int sum) {
+        if (index == a.length) {
+            if (sum == s) {
                 count++;
             }
             return;
         }
 
-        for (int i = start; i < N; i++) {
-            result[depth] = arr[i];
-            go(i + 1, depth + 1, result);
-        }
-    }
-
-    private static void getOutput(int[] result) {
-        var sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            sb.append(result[i]);
-            if (i != M - 1) {
-                sb.append(" ");
-            }
-        }
-        System.out.println(sb);
+        bf(index + 1, sum + a[index]);
+        bf(index + 1, sum);
     }
 }
