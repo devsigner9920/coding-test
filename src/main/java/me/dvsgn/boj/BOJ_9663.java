1832891fa42fea25a7238521f3ed1ca6973a6b9f
devsigner9920 <devsigner9920@icloud.com>
2024-11-15 09:59:16 +0900

    BOJ 2024-11-14

---
+++ src/main/java/me/dvsgn/boj/BOJ_9663.java
@@ -0,0 +1,15 @@
+package me.dvsgn.boj;
+
+import java.util.Scanner;
+
+public class BOJ_9663 {
+    private static int count = 0;
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+
+        var n = Integer.parseInt(sc.next());
+
+        var check = new boolean[n][n];
+
+    }
+}
