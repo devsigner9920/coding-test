174466787d45f34939112d798acd3f5082cbe2bf
devsigner9920 <devsigner9920@icloud.com>
2024-11-29 10:38:41 +0900

    LeetCode, BOJ 2024-11-24

--- src/main/java/me/dvsgn/leetcode/LeetCode_124.java
+++ src/main/java/me/dvsgn/leetcode/LeetCode_124.java
@@ -1,2 +1,39 @@
-package me.dvsgn.leetcode;public class LeetCode_124 {
+package me.dvsgn.leetcode;
+
+public class LeetCode_124 {
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution_leet_124 {
+    private int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxPath(root);
+
+        return max;
+    }
+
+    public int maxPath(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = Math.max(maxPath(root.left), 0);
+        int right = Math.max(maxPath(root.right), 0);
+
+        max = Math.max(max, root.val + left + right);
+
+        return root.val + Math.max(left, right);
+    }
 }
